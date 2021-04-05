71b2868dbf540e8bba0d66d18ce7302afdb82e34
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-04-04 23:54:31 -0700

    countUniqueValues

---
+++ countUniqueValues.js
@@ -0,0 +1,30 @@
+const countUniqueValues = arr => {
+    if (arr.length === 0) return 0;
+    let counter = 0;
+    let map = {};
+    for (let val of arr) {
+        if(map[val]) {
+            continue
+        } else {
+        map[val] = true;
+        counter ++;
+        }
+    }
+    return counter;
+};
+
+const countUniqueValues2 = arr=> {
+    let p1 = 0;
+    let p2 = 1;
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[p1] !== arr[p2]) {
+            counter++
+            p1 = p2;
+            p2++
+        } else if (arr[p1] === arr[p2]) {
+            p2++;
+        }
+    }
+return counter 
+}
