5fbf7d5f9b3df58dde71c58d977ebea7a554c760
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-07-02 05:20:02 -0700

    'Formatting'

--- sumZero.js
+++ sumZero.js
@@ -1,27 +1,27 @@
 const sumZeroImproved = (arr) => {
-    let left = 0;
-    let right = arr.length - 1;
-    while (left < right ) {
-        let sum = arr[left] + arr[right]
-        if (sum === 0) {
-            return [arr[left], arr[right]];
-        } else if (sum > 0 ){
-            right--
-        } else {
-            left++
-        }
-    }
-}
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
 
 const sumZeroMap = (arr) => {
-    let map = {};
-    for (let val of arr) {
-        map[val] = true;
-    };
-    for (let val2 of arr) {
-        let possible = 0 - val2;
-        if (map[possible]) {
-            return [val2, possible]
-        }
-    }
-}
+  let map = {};
+  for (let val of arr) {
+    map[val] = true;
+  }
+  for (let val2 of arr) {
+    let possible = 0 - val2;
+    if (map[possible]) {
+      return [val2, possible];
+    }
+  }
+};
