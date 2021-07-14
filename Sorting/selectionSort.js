9e54570c5c4089a731313eeaa7a9ad36baf77f0b
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-07-13 23:02:26 -0700

    'SelectionSort'

---
+++ Sorting/selectionSort.js
@@ -0,0 +1,31 @@
+// Selection Sort!
+// Places small values into sorted position one at a time. Moving from beginning to the end. Swap position of minimum with beginning of array.
+// Make sure to increase the start of the array by 1.
+// Time Complexity - O(n^2)
+
+const swap = (arr, idx1, idx2) => {
+  const temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
+};
+
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+
+    if (i !== lowest) {
+      swap(arr, i, lowest);
+    }
+  }
+
+  return arr;
+};
+
+let list = [4, 2, 5, 3, 11, 55, 33, 22];
+
+console.log(selectionSort(list));
