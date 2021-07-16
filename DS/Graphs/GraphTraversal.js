bc8ae3f774c58c537897b86a6c42d83d1b9804e8
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-07-16 06:15:49 -0700

    file structure change

---
+++ DS/Graphs/GraphTraversal.js
@@ -0,0 +1,8 @@
+// Visiting/Updating/Checking each vertex in a graph
+
+// *Uses for traversal
+// Finding 'closest' matches/recommendations
+// Shortest path problems
+// - GPS Navigation
+// - Solving mazes
+// - AI (shortest path to win the game)
