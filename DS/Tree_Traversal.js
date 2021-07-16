84392836121f3f23d57e0e759ca0b25470a5d2c1
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-07-16 03:07:04 -0700

    tree traversal

--- DS/Tree_Traversal.js
+++ DS/Tree_Traversal.js
@@ -6,3 +6,8 @@
 // 1. InOrder
 // 2. PreOrder
 // 3. PostOrder
+
+// *Which should you use???
+// In a very wide tree, depth first search will use less space. Using the call stack
+
+// PreOrder can be used to "flatten" out a tree, and is given in an order you can reconstruct the tree!
