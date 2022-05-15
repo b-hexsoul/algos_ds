7cc908ff9826ca14355de22e2760442aef17f77c
b-hexsoul <brandon.hexsel@gmail.com>
2022-05-15 12:10:06 -0700

    'notes on hash tables'

--- DS/HashTable.js
+++ DS/HashTable.js
@@ -3,7 +3,7 @@
 // 2. Chance of collision is low
 // 3. All possible values are utilized approx. equally. Try to distribute uniformly
 
-// Simple Hash Function for strings
+// Simple Hash Function for strings ( NOT perfect );
 function stringHash(key, arrayLen) {
   let total = 0;
   for (let char of key) {
@@ -13,3 +13,18 @@ function stringHash(key, arrayLen) {
   }
   return total;
 }
+
+// Problems with above. ONLY strings. Is NOT o(n). Could be more random..
+// Hash functions almost always take advantage of PRIME NUMBERS.
+
+// Dealing with Collisions...
+// 1. Separate Chaining
+// 2. Linear Probing
+
+// Separate Chaining
+// At each index in our array we store values using a more sophisticated DS like a linked list
+
+// Linear Probing
+// When we find a collision, we search through for the next empty slot
+
+// Hash Table insertion, deletion, access on average is O(1) constant time.
